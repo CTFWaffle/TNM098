bccad5221c9407c35a0d8724e3a21503e0a01b26
Nikita Sidarovich <94785889+LionF4ng@users.noreply.github.com>
2025-05-02 16:02:01 +0200

    Epic

--- Lab3/Lab32.py
+++ Lab3/Lab32.py
@@ -1,4 +1,26 @@
 import pandas  as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-wowie = pd.read_csv('Lab3.2/TNM098-MC3-2011.csv')
+wowie = pd.read_csv('C:/Users/nikit/Documents/GitHub/TNM098/Lab3/Lab3.2/TNM098-MC3-2011.csv', sep=';')
+
+# Renmove punctuation, convert to lowercase remove stop words
+wowie['Content'] = wowie['Content'].str.replace(r'[^\w\s]', '', regex=True).str.lower()
+wowie['Content'] = wowie['Content'].str.split()
+wowie['Content'] = wowie['Content'].apply(lambda x: [word for word in x if word not in ['and', 'the', 'is', 'to', 'a', 'in', 'of', 'for', 'on', 'that', 'this', 'it', 'with']])
+
+# Tokenize the text
+wowie['Content'] = wowie['Content'].apply(lambda x: ' '.join(x))
+
+# Histogram of the temporal data
+plt.figure(figsize=(10, 6))
+plt.hist(wowie['Date'], bins=30, color='blue', alpha=0.7)
+plt.title('Histogram of Temporal Data')
+plt.xlabel('Date')
+plt.ylabel('Frequency')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+
